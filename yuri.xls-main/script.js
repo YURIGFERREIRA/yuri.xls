46a62139381b5fc1e83c4efd81fe307640dd4b29
YURIGFERREIRA <128594336+YURIGFERREIRA@users.noreply.github.com>
2024-06-24 11:20:36 -0300

    Add files via upload

---
+++ yuri.xls-main/script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal=document.querySelector('.caixa-principal');
+const caixaPerguntas=document.querySelector('.caixa-perguntas');
+const caixaAlternativas=document.querySelector('.caixa-alternativas');
+const caixaResultado=document.querySelector('.caixa-resultado');
+const caixaResultado=document.querySelector('.caixa-resultado');
+
+const perguntas=[
+
+    enunciado "perguntas 1".
+    Alternativas [
+        "alternativas 1"
+    ]
+]
