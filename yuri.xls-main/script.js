cba62cb35f4694b0539c9eaab0169bfda3d1fef2
YURIGFERREIRA <128594336+YURIGFERREIRA@users.noreply.github.com>
2024-07-04 08:41:25 -0300

    Add files via upload

--- yuri.xls-main/script.js
+++ yuri.xls-main/script.js
@@ -5,9 +5,39 @@ const caixaResultado=document.querySelector('.caixa-resultado');
 const caixaResultado=document.querySelector('.caixa-resultado');
 
 const perguntas=[
-
-    enunciado "perguntas 1".
-    Alternativas [
-        "alternativas 1"
+{
+    enunciado: "você sabe a possibilidade de ganhar em apostas?",
+    alternativas: [
+         "50%",
+         "não sei"
     ]
-]
+},
+{
+    enunciado:"vocẽ ja chegou a jogar jogo de aposta?",
+    alternativas: [
+        "sim",
+        "não, mas to indo jogar"
+    ]
+},
+{
+    enunciado:"qual foi teu valor ganho em apostas?",
+    alternativas: [
+        "mais de $1000,00",
+        "mais de $100,00"
+    ]
+},
+{
+    enunciado:"conhece alguém viciado em jogos de apostas?",
+    alternativas: [
+        "conheço, é da minha familia",
+        "não conheço"
+    ]
+},
+{
+    enunciado:"perguntas 1",
+    alternativas: [
+        "alternativas 1",
+        "alternativas 2"
+    ]
+},
+];
